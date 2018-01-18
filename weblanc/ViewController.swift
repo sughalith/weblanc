a8c3622f29b9450c69274d6e9f36e0c5c3e95301
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-01-18 20:54:22 +0100

    Initial Commit

---
+++ weblanc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weblanc
+//
+//  Created by Arkadiusz Stankiewicz on 18.01.2018.
+//  Copyright © 2018 Arkadiusz Stankiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
